274f3b44f5273c67bec056e460ef8770e021d2ea
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2025-01-20 14:55:25 +0530

    Add files via upload

---
+++ Day5/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./styles.css"
+function Navbarr() {
+  return (
+    <div>
+        <ul classname="nav-links">
+    <li><a href="#">Dashboard</a></li>
+    <li classname="center"><a href="#">Portfolio</a></li>
+    <li classname="upward"><a href="#">Services</a></li>
+    <li classname="forward"><a href="#">Feedback</a></li>
+  </ul>
+    </div>
+  )
+}
+
+export default Navbarr
