9e2fade93210e0e524a27f9ef6cb730b4851ae5f
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2025-01-23 14:55:48 +0530

    Delete props directory

--- props/Condi.js
+++
@@ -1,9 +0,0 @@
-function StatusMessage({ status }) {
-    if (status === "success") return <p>Operation was successful!</p>;
-    if (status === "error") return <p>There was an error!</p>;
-    if (status === "loading") return <p>Loading...</p>;
-    return null;
-  }
-  
-  export default StatusMessage;
-  
--- props/Default.js
+++
@@ -1,6 +0,0 @@
-function Car({ brand = "Toyota" }) {
-    return <p>Brand: {brand}</p>;
-  }
-  
-  export default Car;
-  
