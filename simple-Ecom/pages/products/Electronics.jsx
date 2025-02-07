509f0b5601e670afb3a8bc5d28e098037af1182e
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2025-02-07 16:43:12 +0530

    Delete simple-Ecom directory

--- simple-Ecom/pages/products/Electronics.jsx
+++
@@ -1,6 +0,0 @@
-const Electronics = () => {
-    return <h1>Electronics Products</h1>;
-  };
-  
-  export default Electronics;
-  
--- simple-Ecom/pages/products/Fashion.jsx
+++
@@ -1,6 +0,0 @@
-const Fashion = () => {
-    return <h1>Fashion Products</h1>;
-  };
-  
-  export default Fashion;
-  
