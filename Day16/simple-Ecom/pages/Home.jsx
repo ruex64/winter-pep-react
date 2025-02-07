4f2333034ddbbcf8f11e85d55c32eecacc3d3a6e
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2025-02-07 16:44:11 +0530

    Add files via upload

---
+++ Day16/simple-Ecom/pages/Home.jsx
@@ -0,0 +1,11 @@
+// const Home = () => {
+//     return <h1>Home Page</h1>;
+//   };
+//   export default Home;
+  
+const Home = () => {
+    return <h1>Welcome to Our E-Commerce Store</h1>;
+  };
+  
+  export default Home;
+  
---
+++ Day16/simple-Ecom/pages/products/Fashion.jsx
@@ -0,0 +1,6 @@
+const Fashion = () => {
+    return <h1>Fashion Products</h1>;
+  };
+  
+  export default Fashion;
+  
